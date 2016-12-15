8a0f5502b637058d7e1f9510365a2a2bf8caa3eb
zhihuacai343 <zhihuacai343@gmail.com>
2016-12-15 07:59:51 -0800

    Create myMap.js

---
+++ myMap.js
@@ -0,0 +1,6 @@
+var map;
+function loadMapScenario() {
+    map = new Microsoft.Maps.Map(document.getElementById('myMap'), {
+        credentials: '7EK3kXDJn5Ow5p7qhM6F~eTVkdKsV-84db9jnnZx3gg~Au919kE9EzGVJFubpBRqtmrDqVs1B3fk96G4FK9y_4lJuQKshCNKODsxj9D0yH9b'
+    });
+}
