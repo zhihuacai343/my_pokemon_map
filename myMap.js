8f7a1b0668bd697a5b1a6192f0f4f88f71c2c33e
zhihuacai343 <zhihuacai343@gmail.com>
2016-12-15 12:23:20 -0800

    Update myMap.js

--- myMap.js
+++ myMap.js
@@ -4,7 +4,7 @@
                 var map = new Microsoft.Maps.Map(document.getElementById('myMap'), {
                     credentials: '7EK3kXDJn5Ow5p7qhM6F~eTVkdKsV-84db9jnnZx3gg~Au919kE9EzGVJFubpBRqtmrDqVs1B3fk96G4FK9y_4lJuQKshCNKODsxj9D0yH9b'
                 });
-                var pushpin = new Microsoft.Maps.Pushpin(map.getCenter(), { icon: 'https://www.bingmapsportal.com/Content/images/poi_custom.png',
+                var pushpin = new Microsoft.Maps.Pushpin(map.getCenter(), { icon: 'https://github.com/chenditc/mypokemon.io/blob/gh-pages/images/pushpin_images/pokemon/1.png',
                     anchor: new Microsoft.Maps.Point(12, 39) });
                 map.entities.push(pushpin);
                 
