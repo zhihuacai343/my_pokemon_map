6646feb6946d1518e2ff26fd0fef19532d82642b
zhihuacai343 <zhihuacai343@gmail.com>
2016-12-15 08:44:36 -0800

    Update myMap.js

--- myMap.js
+++ myMap.js
@@ -2,7 +2,7 @@
 
             function loadMapScenario() {
                 var map = new Microsoft.Maps.Map(document.getElementById('myMap'), {
-                    credentials: 'Your Bing Maps Key'
+                    credentials: '7EK3kXDJn5Ow5p7qhM6F~eTVkdKsV-84db9jnnZx3gg~Au919kE9EzGVJFubpBRqtmrDqVs1B3fk96G4FK9y_4lJuQKshCNKODsxj9D0yH9b'
                 });
                 var pushpin = new Microsoft.Maps.Pushpin(map.getCenter(), { icon: 'https://www.bingmapsportal.com/Content/images/poi_custom.png',
                     anchor: new Microsoft.Maps.Point(12, 39) });
